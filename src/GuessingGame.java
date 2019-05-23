082f74679b079bf0ae775eee01a9ac5ac2a2de8a
Cheng Thao <cheng.thao@ictect.com>
2019-05-22 21:21:22 -0500

    one time guess

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,5 +1,18 @@
 
+import java.util.Scanner;
+
+
 
 public class GuessingGame {
+    
+    public static void main(String[] args){
+        
+        int n = 5;
+        System.out.print("What is the number I'm thinking of?");
+        int answer = new Scanner(System.in).nextInt();
+        if (n==answer){
+            System.out.println("You got it.");
+        }
+    }
 
 }
