5fcb7e7b0668667cb92a7662c9ed7d917311cb8b
Cheng Thao <cthao@csp.edu>
2019-05-29 16:27:02 -0500

    Update GuessingGame.java

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -8,6 +8,7 @@ public class GuessingGame {
     public static void main(String[] args){
         //this number is hard coded!
         //some more comments.
+        //third comments.
         int n = 5;
         System.out.print("What is the number I'm thinking of?");
         int answer = new Scanner(System.in).nextInt();
